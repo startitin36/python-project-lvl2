b7c5f9262c1552e5ed032209303489071d07cb8f
Khasanshin Akhmatnur(Leto_Rus) <hahmatnur@gmail.com>
2021-01-02 22:07:43 +0300

    Step 8 Hexlet check ints in ''

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -41,6 +41,8 @@ def def_value(value):
     if type(value) in simples:
         if value in code_list:
             return value
+        if type(value) == int:
+            return value
         return f'\'{value}\''
     return COMPLEX_VAL
 
