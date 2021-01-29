e9658476198afe239e4d1837acea53430295db05
Khasanshin Akhmatnur(Leto_Rus) <hahmatnur@gmail.com>
2021-01-29 12:10:33 +0300

    extract inner func

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -11,71 +11,86 @@ ADD = PROP + 'added with value: {}'
 REM = PROP + 'removed'
 
 
-def form_view(diffs):
-    """Take diffs of two dicts and form 'plain' view.
+def form_view(diff):
+    """Take diff of two dicts and form 'plain' view.
 
     Arguments:
-        diffs (dict): dict with marked changes between two dicts
+        diff (dict): dict with marked changes between two dicts
 
     Returns:
         string in 'plain' format
     """
-    # create list to accumulate output view lines:
-    lines = []
+    # create list to accumulate output view lines
+    output = []
 
-    # walk throw dict to find changes keys ('removed' and 'added') to form
-    # output view lines list
-    def walk(diff, path):
+    # look for changes though all dict
+    walk(diff, '', output)
+    return '\n'.join(output)
 
-        # process each key/value pair(sorted)
-        for key, value in sorted(diff.items()):
 
-            # look for changes keys in values to process them.
-            # check if key's value type is dict to handle 'get' method
-            if isinstance(value, dict):
+def walk(node, path, lines):
+    """Walk throw node to find changed keys ('removed' and 'added') to append
+    output lines.
 
-                # get values of changes if they are present:
-                val_bef, val_aft = value.get('removed'), value.get('added')
+    Arguments:
+        node (dict): node to search changes in
+        path (str): accumulates keys to change value
+        lines (list): accumulates lines to output
+    """
+    for key, value in sorted(node.items()):
+        # path = make_path(path, key)
+        # look for changes keys in values to process them.
+        # check if key's value type is dict to handle 'get' method
+        if isinstance(value, dict):
 
-                #  no need 'same' key for 'plain' format
-                if 'same' in value:
-                    continue
+            # get values of changes if they are present:
+            val_bef, val_aft = value.get('removed'), value.get('added')
 
-                # if there are both values: before and after changes:
-                elif val_aft is not None and val_bef is not None:
+            #  no need 'same' key for 'plain' format
+            if 'same' in value:
+                continue
 
-                    # mode value if it complex:
-                    val_bef, val_aft = mod_value(val_bef), mod_value(val_aft)
+            # if there are both values: before and after changes:
+            elif val_aft is not None and val_bef is not None:
 
-                    # adding 'updated' line to preparing view:
-                    lines.append(
-                        UPD.format(make_path(path, key), val_bef, val_aft)
-                    )
+                # mode value if it complex:
+                val_bef, val_aft = mod_value(val_bef), mod_value(val_aft)
 
-                # if value before presents and added does not:
-                elif val_bef and 'added' not in value:
+                # adding 'updated' line to preparing view:
+                lines.append(
+                    UPD.format(make_path(path, key), val_bef, val_aft)
+                )
 
-                    # adding 'removed' line to preparing view:
-                    lines.append(REM.format(make_path(path, key)))
+            # if value before presents and added does not:
+            elif val_bef and 'added' not in value:
 
-                # if value after presents and removed does not:
-                elif val_aft and 'removed' not in value:
-                    val_aft = mod_value(val_aft)
+                # adding 'removed' line to preparing view:
+                lines.append(REM.format(make_path(path, key)))
 
-                    # adding 'added' line to preparing view:
-                    lines.append(ADD.format(make_path(path, key), val_aft))
+            # if value after presents and removed does not:
+            elif val_aft and 'removed' not in value:
+                val_aft = mod_value(val_aft)
 
-                # if there is no changes keys in values:
-                else:
+                # adding 'added' line to preparing view:
+                lines.append(ADD.format(make_path(path, key), val_aft))
 
-                    # push value as key and add key to path:
-                    walk(value, make_path(path, key))
+            # if there is no changed keys in values:
+            else:
 
-    walk(diffs, '')
-    return '\n'.join(lines)
+                # push value as  key, add key to path and try walk() again:
+                walk(value, make_path(path, key), lines)
 
 
 def mod_value(value):
+    """ Mod value for output view.
+
+    Arguments:
+        value (any): value to mod
+
+    Returns:
+        value (str): If value is not in (bool, int, float, str)
+     returns [complex value], else 'value'
+    """
     if type(value) in simples:
         if value == 0:
             return f'{value}'
