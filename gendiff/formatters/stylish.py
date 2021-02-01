f361fda7d0803af6bdf969ff7d39e07df6593179
Khasanshin Akhmatnur(Leto_Rus) <hahmatnur@gmail.com>
2021-02-01 17:59:28 +0300

    stylish refact

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -1,4 +1,6 @@
-IND = '  '
+IND = '    '
+MINUS = '  - '
+PLUS = '  + '
 
 
 def form_view(diffs):
@@ -9,7 +11,7 @@ def form_view(diffs):
 
 def walk(diff, lines, lvl):
     lvl += 1
-    indent = IND * 2 * (lvl - 1)
+    indent = IND * (lvl - 1)
     for key, value in sorted(diff.items()):
         if isinstance(value, dict):
             same = value.get('same')
@@ -17,27 +19,27 @@ def walk(diff, lines, lvl):
             removed = value.get('removed')
 
             if same:
-                lines.append(make_line(indent, key, IND * 2, same))
+                lines.append(make_line(indent, key, IND, same))
 
             if removed or removed in ['', 0, 'null']:
                 if isinstance(removed, dict):
-                    lines.append(make_line(indent, key, '  - ', '{'))
+                    lines.append(make_line(indent, key, MINUS, '{'))
                     walk(removed, lines, lvl)
                 else:
-                    lines.append(make_line(indent, key, '  - ', removed))
+                    lines.append(make_line(indent, key, MINUS, removed))
 
             if added or added in ['', 0, 'null']:
                 if isinstance(added, dict):
-                    lines.append(make_line(indent, key, '  + ', '{'))
+                    lines.append(make_line(indent, key, PLUS, '{'))
                     walk(added, lines, lvl)
                 else:
-                    lines.append(make_line(indent, key, '  + ', added))
+                    lines.append(make_line(indent, key, PLUS, added))
 
             if added is None and removed is None and same is None:
-                lines.append(make_line(indent, key, IND * 2, '{'))
+                lines.append(make_line(indent, key, IND, '{'))
                 walk(value, lines, lvl)
         else:
-            lines.append(make_line(indent, key, IND * 2, value))
+            lines.append(make_line(indent, key, IND, value))
     lines.append(make_line(indent, '', '', ''))
 
 
