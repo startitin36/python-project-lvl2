91d8ac6b88a7fd7e3ea321e4501e1ef2d29b5b01
Khasanshin Akhmatnur(Leto_Rus) <hahmatnur@gmail.com>
2021-02-01 18:52:45 +0300

    stylish comlexity decreasing

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -1,6 +1,7 @@
 IND = '    '
 MINUS = '  - '
 PLUS = '  + '
+SC = ': '
 
 
 def form_view(diffs):
@@ -13,38 +14,35 @@ def walk(diff, lines, lvl):
     lvl += 1
     indent = IND * (lvl - 1)
     for key, value in sorted(diff.items()):
+        line1 = indent + IND + key + SC
+
         if isinstance(value, dict):
             same = value.get('same')
             added = value.get('added')
             removed = value.get('removed')
 
             if same:
-                lines.append(make_line(indent, key, IND, same))
+                lines.append(line1 + same)
 
             if removed or removed in ['', 0, 'null']:
+                minus = indent + MINUS + key + SC
                 if isinstance(removed, dict):
-                    lines.append(make_line(indent, key, MINUS, '{'))
+                    lines.append(minus + '{')
                     walk(removed, lines, lvl)
                 else:
-                    lines.append(make_line(indent, key, MINUS, removed))
+                    lines.append(minus + str(removed))
 
             if added or added in ['', 0, 'null']:
+                line = indent + PLUS + key + SC
                 if isinstance(added, dict):
-                    lines.append(make_line(indent, key, PLUS, '{'))
+                    lines.append(line + '{')
                     walk(added, lines, lvl)
                 else:
-                    lines.append(make_line(indent, key, PLUS, added))
+                    lines.append(line + str(added))
 
             if added is None and removed is None and same is None:
-                lines.append(make_line(indent, key, IND, '{'))
+                lines.append(line1 + '{')
                 walk(value, lines, lvl)
         else:
-            lines.append(make_line(indent, key, IND, value))
-    lines.append(make_line(indent, '', '', ''))
-
-
-def make_line(indent, key, change='', value=None):
-    if key:
-        return '{}{}{}: {}'.format(indent, change, key, value)
-    else:
-        return '{}{}{}'.format(indent, '', '}')
+            lines.append(line1 + str(value))
+    lines.append(indent + '}')
